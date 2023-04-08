9b6345644fcfa7172289d1ac3e6f24e72e3f1218
Anna <apradolsv@gmail.com>
2023-04-08 17:16:18 -0300

    ajustando pag busca e adicionando função de upload

--- galeria/views.py
+++ galeria/views.py
@@ -6,6 +6,14 @@ def index(request):
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
-    fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+    fotografias = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografias})
 
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("data").filter(publicada=True)
+    if 'buscar' in request.GET:
+        busca = request.GET['buscar']
+        if busca:
+            fotografias = fotografias.filter(nome__icontains=busca)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
