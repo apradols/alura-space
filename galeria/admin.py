1bb80992fdf36bb0fdcd3dfc94a5e6bdac36ab41
Anna Lívia Prado <75024321+apradols@users.noreply.github.com>
2023-04-07 21:05:34 -0300

    Update admin.py
    fazendo um crud no admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -4,6 +4,6 @@ from galeria.models import Fotografia
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id", "nome")
-    search_fields = ("nome",)
+    search_fields = ("nome",) 
 
 admin.site.register(Fotografia, ListandoFotografias)
